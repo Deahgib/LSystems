a30ab3fdb00fb9bf2d22b3d7a3b3e1dca28f8ab3
Louis Bennette <louis.bennette@gmail.com>
2016-11-13 16:41:04 +0000

    Added file reading to load in axioms and rules from file.

--- octet/src/examples/LSystems/LSystems.h
+++ octet/src/examples/LSystems/LSystems.h
@@ -16,6 +16,66 @@ namespace octet {
     Turtle turtle;
 
     TreeString fractal_code;
+
+    void load_fractal_file(const char* file_name) {
+      std::vector<std::string> settings;
+
+      std::ifstream is(file_name);
+      if (is.bad() || !is.is_open()) return;
+      // store the line here
+      char buffer[2048];
+      // loop over lines
+      while (!is.eof()) {
+        is.getline(buffer, sizeof(buffer));
+        // loop over columns
+        char *b = buffer;
+        //printf("%c\n", *b);
+        if (*b == '#') continue; // Ignore lines starting with #
+
+        for (int col = 0; ; ++col) {
+          char *e = b;
+          while (*e != 0 && *e != ':') ++e;
+
+          // now b -> e contains the chars in a column
+          if (col == 1) {
+            std::string sett = b;
+            settings.push_back(sett);
+          }
+
+          if (*e != ':') break;
+          b = e + 1;
+        }
+      }
+      /*
+      for (int i = 0; i < settings.size(); ++i) {
+        printf("File setting %i: %s\n", (i+1) ,settings[i].c_str());
+      }*/
+
+
+      // Clear previous settings.
+      // Set branches
+      // Set Leaves
+      // Set Axiom
+      fractal_code.set_axiom(settings[2].c_str());
+      // Set rules
+      fractal_code.clear_rules();
+      char *rule_str = const_cast<char*>(settings[3].c_str());
+      char *rules = strtok(rule_str, ">,");
+      while (rules != NULL) {
+        //printf("File rule: %c -> ", *rules);
+        char control = *rules;
+        rules = strtok(NULL, ">,");
+        if (rules == NULL) break; // Error reading the rules line.
+        //printf("%s\n", rules);
+        std::string conv = rules;
+        rules = strtok(NULL, ">,");
+
+        fractal_code.insert_rule(control, conv);
+      }
+      // Set angles
+      
+    }
+
   public:
     /// this is called when we construct the class before everything is initialised.
     LSystems(int argc, char **argv) : app(argc, argv) {
@@ -26,8 +86,12 @@ namespace octet {
       app_scene =  new visual_scene();
       app_scene->create_default_camera_and_lights();
 
-      fractal_code.set_axiom("A");
-      turtle.render(fractal_code.get_string());
+      load_fractal_file("tree_template.txt");
+
+      //fractal_code.set_axiom("0");
+      //fractal_code.clear_rules();
+      //fractal_code.insert_rule('0', "1[0]0");
+      //fractal_code.insert_rule('1', "11");
     }
 
     /// this is called to draw the world
--- octet/src/examples/LSystems/TreeString.h
+++ octet/src/examples/LSystems/TreeString.h
@@ -4,16 +4,35 @@ namespace octet {
   class TreeString {
   private:
     //char* current;
-    //std::map<char*, char*> conversion_table;
+    std::map<char, std::string> conversion_table;
 
     //std::vector<char[]> history;
 
     std::string current;
+
+    bool is_control_char(char ch) {
+      for (std::map<char, std::string>::iterator it = conversion_table.begin(); it != conversion_table.end(); ++it) {
+        //printf(" - Key: %c\n", *it);
+        if (it->first == ch) return true;
+      }
+      return false;
+    }
+
   public:
     TreeString(){}
 
-    void set_axiom(char * axiom) {
+    void set_axiom(const char * axiom) {
+      //printf("Axiom set to %s\n", axiom);
       current = axiom;
+    }
+
+    void clear_rules() {
+      conversion_table.clear();
+    }
+
+    void insert_rule(char key, std::string value) {
+      conversion_table.insert(std::pair<char, std::string>(key, value));
+      //printf("Creating key->val pair: %c -> %s\n", key, conversion_table.find(key)->second.c_str());
     }
 
     const char* get_string() {
@@ -22,8 +41,20 @@ namespace octet {
 
     void do_step() {
       std::string newString;
-      /*for (std::string::iterator itt = current.begin(); itt != current.end(); ++itt) {
-        printf("%c", *itt);
+
+      for (std::string::iterator itt = current.begin(); itt != current.end(); ++itt) {
+        if (is_control_char(*itt)) {
+          //printf("EVOLVE converting %c to %s\n", *itt, conversion_table.find(*itt)->second.c_str());
+          newString += conversion_table.find(*itt)->second;
+        }
+        else {
+          //printf("EVOLVE passing on %c\n", *itt);
+          newString += *itt;
+        }
+      }
+      /*
+      for (std::string::iterator itt = current.begin(); itt != current.end(); ++itt) {
+        //printf("%c", *itt);
 
         if (*itt == '0') {
           newString += "1[0]0";
@@ -34,8 +65,9 @@ namespace octet {
         else {
           newString += *itt;
         }
-      }*/
-
+      }
+      */
+      /*
       for (std::string::iterator itt = current.begin(); itt != current.end(); ++itt) {
         //printf("%c", *itt);
 
@@ -48,48 +80,11 @@ namespace octet {
         else {
           newString += *itt;
         }
-      }
+      }*/
 
       current = newString;
 
       //printf("\nResult: %s\n", newString.c_str());
-      
-      /*
-      char buffer[2048];
-      int buffer_index = 0;
-
-      char* itt = current;
-      for (char ch = *itt; ch != 0; ch = *(++itt)) {
-        printf(" - %c\n", ch);
-        if (ch == '1') {
-          buffer[buffer_index++] = '1';
-          buffer[buffer_index++] = '1';
-        }
-        else if (ch == '0') {
-          buffer[buffer_index++] = '1';
-          buffer[buffer_index++] = '['; 
-          buffer[buffer_index++] = '0';
-          buffer[buffer_index++] = ']'; 
-          buffer[buffer_index++] = '0';
-        }
-        else if(ch == '[') {
-          buffer[buffer_index++] = '[';
-        }
-        else if (ch == ']') {
-          buffer[buffer_index++] = ']';
-        }
-
-      }
-      buffer[buffer_index++] = 0;
-
-      char* itt2 = current;
-      printf("Preparing to render: ");
-      for (char ch = *itt2; ch != 0; ch = *(++itt2)) {
-        printf("%c", ch);
-      }
-      printf("\n");
-
-      */
     }
   };
 }
